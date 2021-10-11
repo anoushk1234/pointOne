0f085e9568541cb9f593ca001fc8347153852714
Anoushk Kharangate <anoushka1828@gmail.com>
2021-10-11 21:40:33 +0530

    made cargo project

---
+++ compiler/target/rls/debug/deps/compiler-b07f5d36a0acb87b.d
@@ -0,0 +1,5 @@
+c:\Users\Anoushk\Documents\pointOne\compiler\target\rls\debug\deps\compiler-b07f5d36a0acb87b.rmeta: src\main.rs
+
+c:\Users\Anoushk\Documents\pointOne\compiler\target\rls\debug\deps\compiler-b07f5d36a0acb87b.d: src\main.rs
+
+src\main.rs:
---
+++ compiler/target/rls/debug/deps/compiler-c720ad96d5b14f2b.d
@@ -0,0 +1,5 @@
+c:\Users\Anoushk\Documents\pointOne\compiler\target\rls\debug\deps\compiler-c720ad96d5b14f2b.rmeta: src\main.rs
+
+c:\Users\Anoushk\Documents\pointOne\compiler\target\rls\debug\deps\compiler-c720ad96d5b14f2b.d: src\main.rs
+
+src\main.rs:
